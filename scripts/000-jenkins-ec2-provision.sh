a1e3e05faca8ddd19b996c139338de12387f3b9d
maxstack <maxstack@null>
2018-05-11 08:38:32 +1200

    removed redundant volume creation line

--- scripts/000-jenkins-ec2-provision.sh
+++ scripts/000-jenkins-ec2-provision.sh
@@ -9,5 +9,4 @@ cd docker-jenkins
 sed -i -e 's/_USERNAME_/admin/g' security.groovy
 sed -i -e 's/_PASSWORD_/somecomplexpassword/g' security.groovy
 sudo docker build -t jenkins:lts-automated .
-sudo docker volume create jenkins_home
 sudo docker run -d -v jenkins_home:/var/jenkins_home --restart unless-stopped --name jenkins -p 8080:8080 -p 50000:50000 jenkins:lts-automated
