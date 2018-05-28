23cd74b0bb9225d6ec17cacf298f573b16491f91
maxstack <maxstack@null>
2018-05-28 12:56:14 +1200

    changes to packer

--- scripts/000-jenkins-ec2-provision.sh
+++ scripts/000-jenkins-ec2-provision.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-sudo yum update -y
-sudo yum install docker git -y
-sudo systemctl enable docker
-sudo systemctl start docker
-git clone https://github.com/maxstack/docker-jenkins.git
-cd docker-jenkins
-sed -i -e 's/_USERNAME_/admin/g' security.groovy
-sed -i -e 's/_PASSWORD_/somecomplexpassword/g' security.groovy
-sudo docker build -t jenkins:lts-automated .
-sudo docker run -d -v jenkins_home:/var/jenkins_home --restart unless-stopped --name jenkins -p 8080:8080 -p 50000:50000 jenkins:lts-automated
+sudo yum update -y \
+&& sudo yum install docker git -y \
+&& sudo systemctl enable docker \
+&& sudo systemctl start docker \
+&& git clone https://github.com/maxstack/docker-jenkins.git \
+&& cd docker-jenkins \
+&& sed -i -e 's/_USERNAME_/admin/g' security.groovy \
+&& sed -i -e 's/_PASSWORD_/somecomplexpassword/g' security.groovy \
+&& sudo docker build -t jenkins:lts-automated . \
+&& sudo docker run -d -v jenkins_home:/var/jenkins_home -v /var/run/docker.sock:/var/run/docker.sock --restart unless-stopped --name jenkins -p 8080:8080 -p 50000:50000 jenkins:lts-automated
